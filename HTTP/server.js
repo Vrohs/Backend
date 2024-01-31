98bca41aaf9c905e50f2f75bd1b820360a970baa
Vrohs <vivekanand1469.be22@chitkarauniversity.edu.in>
2024-01-31 10:59:04 +0530

    new route created for style.css

--- HTTP/server.js
+++ HTTP/server.js
@@ -9,33 +9,45 @@ const server = http.createServer((req, res) => {
     // res.end()
     // console.log(req.url)
 
-    if(req.url == "/home.html"){
+    if (req.url == "/home.html") {
 
         fs.readFile("./home.html", "utf-8", (err, data) => {
 
-            if(err){
+            if (err) {
 
                 res.write("hmmm...can't reach page 🙂")
             } else {
 
                 res.write(data)
-                res.end()
             }
+            res.end()
         })
-      
+    }
+
+        if (req.url == "/style.css") {
+
+            fs.readFile("./style.css", "utf-8", (err, data) => {
+
+                if (err) {
+
+                    res.write("Hmmm...")
+                } else {
+
+                    res.write(data)
+                }
+                res.end()
+            })
+        }
+    })
+
         // res.write("<h1> home page, you succesfully reached homepage 🌞</h1>")
         // res.end()
-    } else {
 
-        res.write("unknown")
-        res.end()
-    }
-})
 
 
 const port = 3000;
 
 server.listen(port, (err) => {
 
-   console.log(`server is running at port: ${port}`)
+    console.log(`server is running at port: ${port}`)
 })

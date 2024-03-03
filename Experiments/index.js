86d7b7c8b3e243309b0e16b56a8f76f145a09920
Vivek Rohtasvi <82621421+Vrohs@users.noreply.github.com>
2024-03-03 05:36:14 +0000

    dierctory structure changed

---
+++ Experiments/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+
+
+const newServer = http.createServer((req, res) => {
+
+    console.log('new req. receieved!')
+    res.end('Hello')
+})
+
+
+
+const port = 3000
+
+
+
+newServer.listen(port, () => {
+
+    console.log('server stsrted')
+})
+
+
